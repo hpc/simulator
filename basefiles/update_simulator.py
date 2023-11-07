b672245548a94c9d654768c81d94e8b5c368d113
Craig Walker <cswalke1@coastal.edu>
2023-11-07 18:03:55 -0500

    added update_simulator.py

---
+++ basefiles/update_simulator.py
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd $prefix
+cp $prefix/basefiles/batsim_environment.sh ../
+git pull
+rm $prefix/basefiles/batsim_environment.sh
+mv ../batsim_environment.sh $prefix/basefiles/batsim_environment.sh
